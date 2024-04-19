949836baa08d91e3749187f83bc1a2aa804132c8
Dylan <dylanquq@gmail.com>
2024-04-20 04:06:38 +0800

    003 NACOS作为配置中心,gulimall-coupon.properties

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.coupon.entity.CouponEntity;
 import com.atguigu.gulimall.coupon.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -19,11 +21,22 @@ import java.util.Map;
  * @email Dylanquq@gmail.com
  * @date 2024-04-19 23:54:14
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private int age;
+
+    @GetMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
 
     @RequestMapping("/member/list")
     public R memberCoupon() {
